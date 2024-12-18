73f6d0c382167a658aa4fe470e2db87dab93d78b
mounika <mounikabodduru2001@gmail.com>
2024-12-18 15:31:29 +0530

    Spelling correction

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
             <div className='p-4 flex items-center justify-between'>
                 <div className='flex items-center gap-3'>
                     <img className='w-8' src={assets.stack_icon} alt="" />
-                    <p className='font-semibold'>Your Librar</p>
+                    <p className='font-semibold'>Your Library</p>
                 </div>
                 <div className='flex items-center gap-3'>
                     <img className='w-5' src={assets.arrow_icon} alt="" />
@@ -32,7 +32,7 @@ const Sidebar = () => {
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                 <h1>Create Your 1st Playlist</h1>
                 <p className='font-light'>it's easy we will help you</p>
-                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Platlist</button>
+                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
             </div>
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
                 <h1>Let's find some posdcast to follow</h1>
